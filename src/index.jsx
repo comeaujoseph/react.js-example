69e7391dfb65d82e41c7437f96bf117e706b4ac8
Joseph Comeau <comeau.joey@gmail.com>
2020-02-23 13:08:59 -0800

    example app

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,10 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-import App from './App';
-
-ReactDOM.render(
-    <App />,
-    document.getElementById('app')
+const App = () => (
+    <div>
+        <h1>Hello World!</h1>
+    </div>
 );
+
+ReactDOM.render(<App />, document.getElementById('app'));
